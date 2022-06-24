5d220e0960b92562c02795cac01397f38e11745c
Czarina Sotelo <sotelocz@sheridancollege.ca>
2022-06-24 14:07:37 -0400

    Added Comments into new java class, Czarina_Sotelo.java

---
+++ src/midtermexam_versiona_extensioncode/CzarinaSotelo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author sotel
+ */
+public class CzarinaSotelo {
+    /**
+     * This are comments for Question 4
+     * Midterm Part C 
+     * SYST 17796
+     */
+}
